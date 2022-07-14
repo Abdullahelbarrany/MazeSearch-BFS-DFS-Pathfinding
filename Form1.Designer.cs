5cafa435f09be21f19c8c94f8c1411f2a989ff04
Abdullahelbarrany <93045153+Abdullahelbarrany@users.noreply.github.com>
2022-07-14 23:07:27 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,389 @@
+﻿
+namespace FinalMaze_ISA
+{
+    partial class Maze
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.board = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column16 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column17 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column18 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column19 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column20 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.algorithmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dFSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bFSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uNIFORMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.board)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // board
+            // 
+            this.board.AllowUserToAddRows = false;
+            this.board.AllowUserToDeleteRows = false;
+            this.board.AllowUserToResizeColumns = false;
+            this.board.AllowUserToResizeRows = false;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.board.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.board.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.board.ColumnHeadersVisible = false;
+            this.board.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column15,
+            this.Column16,
+            this.Column17,
+            this.Column18,
+            this.Column19,
+            this.Column20,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10,
+            this.Column11,
+            this.Column12,
+            this.Column13,
+            this.Column14});
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.Padding = new System.Windows.Forms.Padding(1);
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.board.DefaultCellStyle = dataGridViewCellStyle4;
+            this.board.Location = new System.Drawing.Point(0, 31);
+            this.board.Name = "board";
+            this.board.ReadOnly = true;
+            this.board.RowHeadersVisible = false;
+            this.board.RowHeadersWidth = 51;
+            this.board.RowTemplate.Height = 24;
+            this.board.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.board.Size = new System.Drawing.Size(783, 642);
+            this.board.TabIndex = 0;
+            this.board.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.board_CellClick);
+            this.board.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 125;
+            // 
+            // Column15
+            // 
+            this.Column15.HeaderText = "Column15";
+            this.Column15.MinimumWidth = 6;
+            this.Column15.Name = "Column15";
+            this.Column15.ReadOnly = true;
+            this.Column15.Width = 125;
+            // 
+            // Column16
+            // 
+            this.Column16.HeaderText = "Column16";
+            this.Column16.MinimumWidth = 6;
+            this.Column16.Name = "Column16";
+            this.Column16.ReadOnly = true;
+            this.Column16.Width = 125;
+            // 
+            // Column17
+            // 
+            this.Column17.HeaderText = "Column17";
+            this.Column17.MinimumWidth = 6;
+            this.Column17.Name = "Column17";
+            this.Column17.ReadOnly = true;
+            this.Column17.Width = 125;
+            // 
+            // Column18
+            // 
+            this.Column18.HeaderText = "Column18";
+            this.Column18.MinimumWidth = 6;
+            this.Column18.Name = "Column18";
+            this.Column18.ReadOnly = true;
+            this.Column18.Width = 125;
+            // 
+            // Column19
+            // 
+            this.Column19.HeaderText = "Column19";
+            this.Column19.MinimumWidth = 6;
+            this.Column19.Name = "Column19";
+            this.Column19.ReadOnly = true;
+            this.Column19.Width = 125;
+            // 
+            // Column20
+            // 
+            this.Column20.HeaderText = "Column20";
+            this.Column20.MinimumWidth = 6;
+            this.Column20.Name = "Column20";
+            this.Column20.ReadOnly = true;
+            this.Column20.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 125;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column5";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 125;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Column6";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 125;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Column7";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            this.Column7.Width = 125;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Column8";
+            this.Column8.MinimumWidth = 6;
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            this.Column8.Width = 125;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Column9";
+            this.Column9.MinimumWidth = 6;
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
+            this.Column9.Width = 125;
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "Column10";
+            this.Column10.MinimumWidth = 6;
+            this.Column10.Name = "Column10";
+            this.Column10.ReadOnly = true;
+            this.Column10.Width = 125;
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "Column11";
+            this.Column11.MinimumWidth = 6;
+            this.Column11.Name = "Column11";
+            this.Column11.ReadOnly = true;
+            this.Column11.Width = 125;
+            // 
+            // Column12
+            // 
+            this.Column12.HeaderText = "Column12";
+            this.Column12.MinimumWidth = 6;
+            this.Column12.Name = "Column12";
+            this.Column12.ReadOnly = true;
+            this.Column12.Width = 125;
+            // 
+            // Column13
+            // 
+            this.Column13.HeaderText = "Column13";
+            this.Column13.MinimumWidth = 6;
+            this.Column13.Name = "Column13";
+            this.Column13.ReadOnly = true;
+            this.Column13.Width = 125;
+            // 
+            // Column14
+            // 
+            this.Column14.HeaderText = "Column14";
+            this.Column14.MinimumWidth = 6;
+            this.Column14.Name = "Column14";
+            this.Column14.ReadOnly = true;
+            this.Column14.Width = 125;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.algorithmToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(786, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // algorithmToolStripMenuItem
+            // 
+            this.algorithmToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dFSToolStripMenuItem,
+            this.bFSToolStripMenuItem,
+            this.uNIFORMToolStripMenuItem,
+            this.aToolStripMenuItem});
+            this.algorithmToolStripMenuItem.Name = "algorithmToolStripMenuItem";
+            this.algorithmToolStripMenuItem.Size = new System.Drawing.Size(90, 24);
+            this.algorithmToolStripMenuItem.Text = "Algorithm";
+            // 
+            // dFSToolStripMenuItem
+            // 
+            this.dFSToolStripMenuItem.Name = "dFSToolStripMenuItem";
+            this.dFSToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.dFSToolStripMenuItem.Text = "DFS";
+            this.dFSToolStripMenuItem.Click += new System.EventHandler(this.dFSToolStripMenuItem_Click);
+            // 
+            // bFSToolStripMenuItem
+            // 
+            this.bFSToolStripMenuItem.Name = "bFSToolStripMenuItem";
+            this.bFSToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.bFSToolStripMenuItem.Text = "BFS";
+            // 
+            // uNIFORMToolStripMenuItem
+            // 
+            this.uNIFORMToolStripMenuItem.Name = "uNIFORMToolStripMenuItem";
+            this.uNIFORMToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.uNIFORMToolStripMenuItem.Text = "UNIFORM";
+            // 
+            // aToolStripMenuItem
+            // 
+            this.aToolStripMenuItem.Name = "aToolStripMenuItem";
+            this.aToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.aToolStripMenuItem.Text = "A*";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Maze
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 678);
+            this.Controls.Add(this.board);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Maze";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.board)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView board;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column15;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column16;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column17;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column18;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column19;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column20;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column14;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem algorithmToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dFSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bFSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uNIFORMToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalMaze_ISA
+{
+    public partial class Maze : Form
+    {
+        public Maze()
+        {
+            InitializeComponent();
+            this.Load += Maze_Load;
+         
+
+        }
+
+     
+
+        int i, j;
+        private void Maze_Load(object sender, EventArgs e)
+        {
+            board.BackgroundColor = Color.White;
+            board.DefaultCellStyle.BackColor = Color.White;
+
+            for (i = 0; i < 21; i++)
+            {
+                board.Rows.Add();
+            }
+            foreach (DataGridViewColumn c in board.Columns)
+                c.Width = board.Width / board.Columns.Count;
+            // formatcell(5, 2);
+            MessageBox.Show("Click on the Hero's Location");
+        }
+        visited ptrav;
+        visited pnn;
+
+        void dfs()
+        {
+            
+                pnn = new visited();
+                pnn.r = sr;
+                pnn.c = sc;
+                pnn.parentc = 0;
+                pnn.parentr = 0;
+                queue.Add(pnn);
+                ptrav = new visited();
+                ptrav.r = sr;
+                ptrav.c = sc;
+                int y = 0;
+                int first = 0;
+                closed.Add(ptrav);
+                //MessageBox.Show("" + board.Rows[sr].Cells[sc].Tag);
+                while (queue.Count != 0)
+                {
+                    if (y == queue.Count)
+                    {
+                        //  MessageBox.Show("3ash bruh");
+
+                        break;
+                    }
+                    r = queue[y].r;
+                    c = queue[y].c;
+                    if (r == sr && c == sc)
+                    { }
+                    else
+                    { board.Rows[r].Cells[c].Style.BackColor = Color.Cyan; }
+
+                    if (goal == 1)
+                    { }
+                    else
+                    { explorenodes(r, c); }
+                    // nodesleft--;
+                    //     if (nodesleft == 0)
+                    //    {
+                    //       nodesleft = nodesnext;
+                    //        nodesnext = 0;
+                    //        mc++;
+
+                    ///    }
+
+
+                    y++;
+                }
+            }
+        void bfs()
+        {
+             pnn= new visited();
+            pnn.r = sr;
+            pnn.c = sc;
+            pnn.parentc = 0;
+            pnn.parentr = 0;
+            queue.Add(pnn);
+            ptrav = new visited();
+            ptrav.r = sr;
+            ptrav.c = sc;
+            int y=0;
+            int first = 0;
+            closed.Add(ptrav);
+             //MessageBox.Show("" + board.Rows[sr].Cells[sc].Tag);
+            while (queue.Count !=0)
+            { 
+                if (y == queue.Count)
+                {
+                  //  MessageBox.Show("3ash bruh");
+                   
+                    break;
+            }
+                r = queue[y].r;
+                c = queue[y].c;
+                if (r == sr && c == sc)
+                { }
+                else
+                { board.Rows[r].Cells[c].Style.BackColor = Color.Cyan;}
+
+                if (goal == 1)
+                { }
+                else
+                { explorenodes(r, c); }
+                // nodesleft--;
+                //     if (nodesleft == 0)
+                //    {
+                //       nodesleft = nodesnext;
+                //        nodesnext = 0;
+                //        mc++;
+
+                ///    }
+
+                
+                 y++; 
+            }
+            int gr, gc,b=0;
+            visited goalpath =queue[queue.Count -1];
+            if (goal == 1)
+            { while (b==0)
+                {
+                   gr= goalpath.r;
+                    gc =goalpath.c;
+                    board.Rows[gr].Cells[gc].Style.BackColor = Color.Gold;
+                    for (j = 0; j < queue.Count; j++)
+                    {if (goalpath.parentr == queue[j].r && goalpath.parentc == queue[j].c)
+                        {
+                            goalpath = queue[j];
+                        }
+                    
+                    }
+                    if (goalpath.parentr == 0)
+                    {
+                        break; 
+                    }
+
+
+                }
+            
+            
+            }
+          
+        
+        }
+        void explorenodes(int r, int c)
+        {
+            int rr,ct=0;
+            int cc;
+        
+            for (i = 0; i < 4; i++)
+            {
+                rr = r + dr[i];
+                cc = c + dc[i];
+                if (rr > 19 || rr < 0 || cc > 19 || cc < 0)
+                {
+                    ct++;
+
+                }
+                else
+                {
+                    for (j = 0; j < obstacles.Count; j++)
+                    {
+                        if (rr == obstacles[j].r && cc == obstacles[j].c)
+                        {
+                            ct++;
+                            // MessageBox.Show("obstacle");
+                        }
+                    }
+                    for (j = 0; j < closed.Count; j++)
+                    {
+                        if (rr == closed[j].r)
+                        {
+                            if (cc == closed[j].c)
+                            { ct++; }
+                        }
+                    }
+
+                    if (rr==g.r&&cc==g.c)
+                    {
+                        goal = 1;
+                      ///  MessageBox.Show("Goal");
+                        break;
+                    }
+                    if (ct == 0)
+                    {
+                        pnn = new visited();
+                        pnn.r = rr;
+                        pnn.c = cc;
+                        pnn.parentc = c;
+                        pnn.parentr = r;
+                        queue.Add(pnn);
+                        ///  nodesnext++;
+                        closed.Add(pnn);
+                    }
+                }
+                ct = 0;   
+            }
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+        }
+        int goal = 0;
+        int counter = 0,mc,nodesleft=1,nodesnext,c,r;
+        int[] dr = { -1, 1, 0, 0 };
+        List<visited> closed = new List<visited>();
+        visited g;
+        List<visited> obstacles = new List<visited>();
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        int[] dc = { 0, 0, 1, -1 };
+        int sc, sr;
+        public class visited
+        {   public int r=0, c=0, parentr=0, parentc=0,heu=0,cost=0;
+        }
+
+        private void dFSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bfs();
+
+        }
+
+        List<visited> queue = new List<visited>();
+      //cell tag will be the visited node[s]
+        private void board_CellClick(object sender, DataGridViewCellEventArgs e)
+        {if (counter == 0)
+            {
+                DataGridViewCellStyle cell = new DataGridViewCellStyle();
+                cell.BackColor = Color.Blue;
+                cell.Tag = 3;
+                board.Rows[e.RowIndex].Cells[e.ColumnIndex].Style = cell;
+                counter++;
+                sc = e.ColumnIndex;
+                sr = e.RowIndex;
+              //  MessageBox.Show("" + sc + "   " + sr);
+                MessageBox.Show("Click On The Goal's Location");
+            }
+            else if (counter == 1)
+            {
+                DataGridViewCellStyle cell = new DataGridViewCellStyle();
+                cell.BackColor = Color.Green;
+                cell.Tag = 1;
+                g = new visited();
+                g.r = e.RowIndex;
+                g.c = e.ColumnIndex;
+                board.Rows[e.RowIndex].Cells[e.ColumnIndex].Style = cell;
+                counter++;
+                MessageBox.Show("Place the Obstacles");
+            }
+            else if (counter > 1)
+            {
+                DataGridViewCellStyle cell = new DataGridViewCellStyle();
+                cell.BackColor = Color.Black;
+                cell.Tag = 2;
+                pnn = new visited();
+                pnn.r = e.RowIndex;
+                pnn.c = e.ColumnIndex;
+                obstacles.Add(pnn);
+                board.Rows[e.RowIndex].Cells[e.ColumnIndex].Style = cell;
+                counter++;
+            }
+           
+        }
+      
+        //we can make parent a matrix and falg as colour
+
+      
+    }
+}
